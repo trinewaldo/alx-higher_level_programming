e44149d22aa0f64c67e0a799d21f8126954a4339
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2024-01-02 16:50:23 +0300

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end="")
+for a in range(ord('a'), ord('z') + 1):
+    if a != ord('q') and a != ord('e'):
+        print("{:c}".format(a), end="")
