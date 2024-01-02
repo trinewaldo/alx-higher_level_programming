08623bab65ffc87c787a7463027bb7d99af339e3
Trine Waldo <trinewaldo@gmail.com>
2024-01-02 00:54:04 -0800

    Create a new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable \
+ power with very clear syntax"
+print(str[42:92])
+
